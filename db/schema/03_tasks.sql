f5da094fa70834f7beab6377f0f930ae9ea5685f
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-04-07 22:52:02 -0400

    modified table and entries

--- db/schema/03_tasks.sql
+++ db/schema/03_tasks.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS tasks CASCADE;
 
 CREATE TABLE tasks (
     task_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES users(user_id),
+    user_id INT NOT NULL REFERENCES users(id),
     cat_id INT NOT NULL REFERENCES categories(category_id),
     priority INT DEFAULT 0 CHECK (priority BETWEEN 0 AND 5),
-    title VARCHAR(255),
-    time_due TIMESTAMP,
+    title VARCHAR(255) NOT NULL,
+    task_due TIMESTAMP,
     is_completed BOOLEAN DEFAULT FALSE,
-    start_date TIMESTAMP,
-    end_date TIMESTAMP
+    created_date TIMESTAMP,
+    completed_date TIMESTAMP
 );
