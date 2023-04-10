696c62e30dc6a5b9028a4be7e3bd20faa83108c4
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-04-10 19:41:15 -0400

    Added the routes

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  console.log('categories');
-  res.render('categories');
+// get the category List
+router.get("/:cat_id", (req, res) => {
+  console.log("categories");
+  getTasksByCategories(cat_id);
+  res.render("categories");
 });
+
+// Editing a task
+router.post("/:task_id", (req, res) => {
+  //const newtask = editTask(req.body);
+  res.send("categories");
+});
+
+//Deleting the task
+
+//Editing the Task
 
 module.exports = router;
