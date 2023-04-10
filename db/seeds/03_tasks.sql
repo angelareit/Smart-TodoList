cd9b99dee31b45e8bb5b199a041dd5303eb60b17
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-04-10 12:19:50 -0400

    Modified Entries for 03_tasks.sql

--- db/seeds/03_tasks.sql
+++ db/seeds/03_tasks.sql
@@ -1,14 +1,7 @@
-INSERT INTO tasks (user_id, cat_id, priority, title, task_due, created_date)
-VALUES (1, 2, 3, 'Finish project', '2023-04-30 17:00:00', CURRENT_TIMESTAMP);
-
-INSERT INTO tasks (user_id, cat_id, priority, title, task_due, created_date)
-VALUES (2, 1, 2, 'Write blog post', '2023-04-10 09:00:00', CURRENT_TIMESTAMP);
-
-INSERT INTO tasks (user_id, cat_id, priority, title, task_due, created_date)
-VALUES (3, 3, 1, 'Do laundry', '2023-04-12 12:00:00', CURRENT_TIMESTAMP);
-
-INSERT INTO tasks (user_id, cat_id, title, task_due, created_date)
-VALUES (1, 2, 'Schedule dentist appointment', '2023-04-15 14:00:00', CURRENT_TIMESTAMP);
-
-INSERT INTO tasks (user_id, cat_id, priority, title, task_due, created_date)
-VALUES (2, 1, 4, 'Prepare for presentation', '2023-04-20 10:00:00', CURRENT_TIMESTAMP);
+INSERT INTO tasks (user_id, cat_id, priority, title, task_due, is_completed, created_date, completed_date)
+VALUES
+  (1, 2, 3, 'Buy groceries', '2023-04-12 12:00:00', FALSE, '2023-04-10 09:00:00', NULL),
+  (2, 1, 2, 'Watch movie', '2023-04-13 20:00:00', FALSE, '2023-04-10 14:00:00', NULL),
+  (3, 3, 4, 'Read book', '2023-04-14 18:00:00', FALSE, '2023-04-10 16:00:00', NULL),
+  (4, 5, 1, 'Cook dinner', '2023-04-15 19:00:00', FALSE, '2023-04-10 18:00:00', NULL),
+  (5, 4, 5, 'Buy new shoes', '2023-04-16 14:00:00', FALSE, '2023-04-10 20:00:00', NULL);
