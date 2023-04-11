98f9d8a9c9387efe6828240dbf9e2fc7d0cad0d3
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-04-11 10:05:37 -0400

    Corrected the path

--- routes/home.js
+++ routes/home.js
@@ -1,33 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getTasks, getTaskByTitle } = require('../db/queries/tasks');
-const addTasks = require("../public/scripts/addtasks.js");
+const { getTasks, getTaskByTitle } = require("../db/queries/tasks");
+const addTasks = require("../db/queries/addtasks.js");
 
 const data = [
-  { id: 1, text: 'Hello There' },
-  { id: 2, text: 'Hello Ang' },
+  { id: 1, text: "Hello There" },
+  { id: 2, text: "Hello Ang" },
 ];
 
-
-router.get('/', (req, res) => {
-  console.log('got home');
-  res.render('home');
+router.get("/", (req, res) => {
+  console.log("got home");
+  res.render("home");
 });
 
-router.get('/new', (req, res) => {
-  console.log('got home');
+router.get("/new", (req, res) => {
+  console.log("got home");
   /*   getTaskByTitle();
-  */
-  res.send({data});
+   */
+  res.send({ data });
 });
 
-router.post('/', (req, res) => {
-  console.log('HELLO HOME!');
-  res.render('home');
+router.post("/", (req, res) => {
+  console.log("HELLO HOME!");
+  res.render("home");
 });
 
 router.post("/new", (req, res) => {
   const newtask = addTasks(req.body);
-  res.send("Hello World");});
-  
+  res.send("Hello World");
+});
+
 module.exports = router;
