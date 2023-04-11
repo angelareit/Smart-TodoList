bcd8efce03cc766ab1c10ec1e90efff325faae8f
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-04-11 19:35:03 -0400

    Removed Unwanted comments

--- db/schema/02_categories.sql
+++ db/schema/02_categories.sql
@@ -4,16 +4,3 @@ CREATE TABLE categories (
     category_id INT PRIMARY KEY,
     category_name VARCHAR(255)
 );
-
-
-/* const arrayHashmap = originalArray.reduce((obj, item) => {
-  obj[
-item.id
-] ? obj[
-item.id
-].elements.push(...item.elements) : (obj[
-item.id
-] = { ...item });
-  return obj;
-}, {});
- */
