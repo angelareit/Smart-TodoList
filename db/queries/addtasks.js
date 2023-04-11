ae12fb536f0228e93e4f5b2f9a42290b6c4e3985
ashwinihegde28 <ashwinihegde28@gmail.com>
2023-04-11 11:28:15 -0400

    added CategorizeTasks function

--- db/queries/addtasks.js
+++ db/queries/addtasks.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-
+const categorizeTasks = require("../../public/scripts/categorizeTask.js");
 //create a new pool for the database
 const pool = new Pool({
   user: "labber",
@@ -9,19 +9,25 @@ const pool = new Pool({
 });
 
 // Add new task to database
-const addTasks = function (req) {
-  const date = new Date();
+const addTasks = function (reqBody) {
+  //check if the category id is given else call categorize
+  const cat_id = reqBody.cat_id;
+  if (!cat_id) {
+    // categorize the task if cat_id is not provided
+    cat_id = categorizeTasks(reqBody.title);
+  }
+  // Query the database
   const query = `
-		INSERT INTO tasks (user_id, cat_id, priority,title, task_due,)
+	INSERT INTO tasks (user_id, cat_id, priority,title, task_due,)
   	VALUES ($1, $2, $3, $4, $5)
   	RETURNING *;`;
   const values = [
-    req.body.user_id,
-    req.body.cat_id,
-    req.body.user_id,
-    req.body.priority,
-    req.body.title,
-    req.body.task_due,
+    reqBody.user_id,
+    reqBody.cat_id,
+    reqBody.user_id,
+    reqBody.priority,
+    reqBody.title,
+    reqBody.task_due,
   ];
   return pool
     .query(query, values)
