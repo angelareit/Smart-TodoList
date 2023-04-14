d0f90b59035515fcb825f3c2064e9df9c74cb73e
angelareit <anreit01@email.com>
2023-04-14 01:22:27 +0000

    added missing import on categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const taskQueries = require("../db/queries/tasks");
+const categoryQueries = require("../db/queries/categories");
 const { markComplete } = require("../db/queries/helper");
 const { categorizeTask,
   categorizeTasksByAPI } = require("../public/scripts/categorizeTask");
